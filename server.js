d7fefae7bc1a74dd67f890a7cd9f743768411fde
Nero <neroshan12@gmail.com>
2017-12-14 11:15:14 +0000

    Add news feed

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const request = require('request');
-app.set('view engine', 'ejs')
-
+app.set('view engine', 'ejs');
 
 MongoClient.connect(
   'mongodb://mm_recruitment_user_readonly:rebelMutualWhistle@ds037551.mongolab.com:37551/mm-recruitment',
@@ -17,16 +16,36 @@ MongoClient.connect(
 );
 
 app.get('/', function(req, res) {
-  request({
-    url: 'http://mm-recruitment-stock-price-api.herokuapp.com/company/MSFT',
-    json: true
-  }, (error, response, body) => {
-  db.collection("company").find().toArray(function(err, results) {
-    if (err) return console.log(err)
-    res.render("index.ejs", {company: results, latestPrice: body.latestPrice})
-    });
-  })
+  request(
+    {
+      url: 'http://mm-recruitment-stock-price-api.herokuapp.com/company/MSFT',
+      json: true
+    },
+    function(error, response, body) {
+      db
+        .collection('company')
+        .find()
+        .toArray(function(err, results) {
+          if (err) return console.log(err);
+          res.render('index.ejs', {
+            company: results,
+            latestPrice: body.latestPrice
+          });
+        });
+    }
+  );
 });
 
-app.get('/', function(req, res) {
-})
+app.get('/body', function(req, res) {
+  request(
+    {
+      url: 'http://mm-recruitment-story-feed-api.herokuapp.com/4934',
+      json: true
+    },
+    function(error, response, hash) {
+      if (error) return console.log(error);
+      console.log(hash[0].body);
+      res.render('index2.ejs', { test: hash[0].body });
+    }
+  );
+});
